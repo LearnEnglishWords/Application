9203c14b560ad0cc7cc069bfa780598f53371a42
Martin Jablecnik <martin.jablecnik@email.cz>
2020-02-05 22:28:08 +0100

    init

---
+++ src/components/App.svelte
@@ -0,0 +1,45 @@
+<!-- App.svelte -->
+<App params={f7params}>
+    <!-- Current View/Router, initial page will be loaded from home.svelte component -->
+      <View main url="/" />
+</App>
+<script>
+	// Import pages components
+  import { onMount } from 'svelte';
+  import cordovaApp from '../js/cordova-app';
+	import CollectionListView from '../views/CollectionListView.svelte';
+  import { f7, f7ready, App, Views, View } from 'framework7-svelte';
+  import { Device }  from 'framework7/framework7-lite.esm.bundle.js';
+
+  console.log("Mounted ;)");
+
+  onMount(() => {
+    f7ready(() => {
+      // Init cordova APIs (see cordova-app.js)
+      if (Device.cordova) {
+        cordovaApp.init(f7);
+      }
+      // Call F7 APIs here
+    });
+  })
+
+  const f7params = {
+    id: 'eu.learn.english.words',
+    name: 'LearnEnglishWords',
+    theme: 'auto', // Automatic theme detection
+    input: {
+      scrollIntoViewOnFocus: Device.cordova && !Device.electron,
+      scrollIntoViewCentered: Device.cordova && !Device.electron,
+    },
+    // Cordova Statusbar settings
+    statusbar: {
+      androidOverlaysWebView: true,
+    },
+    routes: [
+      {
+        path: '/',
+        component: CollectionListView
+      }
+    ]
+  };
+</script>
---
+++ src/views/CollectionListView.svelte
@@ -0,0 +1,45 @@
+<Page name="home">
+  <Navbar title="LearnEnglishWords">
+  <Subnavbar>
+    Vyberte kolekci slovicek:
+  </Subnavbar>
+  </Navbar>
+
+
+  <List accordionList mediaList inset>
+    {#each collectionItems as {label, text, description, disabled}, id}
+      <ListItem accordionItem title="{label}" text="{text}">
+        <AccordionContent>
+          <Block>
+            <p>
+              {description}
+            </p>
+          </Block>
+        </AccordionContent>
+      </ListItem>
+    {/each}
+  </List>
+</Page>
+
+<script>
+  import { 
+    Page, 
+    AccordionContent, 
+    Navbar,
+    Subnavbar,
+    Link,
+    List,
+    ListItem,
+    Block 
+  } from 'framework7-svelte';
+
+  const collectionItems = [
+    {label: "Basic", text: "(Learn 1000 words)", description: "Obsahuje vsechna zakladni anglicka slovicka pro zakladni komunikaci a dorozumeni. ", disabled: false},
+    {label: "Standard", text: "(Learn 3000 words)", description: "Se znalosti 2500 az 3000 anglickych slov dokážete porozumět 90 % každodenní anglické konverzace, anglicky psaným novinám a časopisům.", disabled: true},
+    {label: "Student", text: "(Learn 5000 words)", description: "Specialni kolekce pro studenty. Obsahuje slovicka serazena do skupin podle lekci nejznamejsich ucebnic.", disabled: true},
+    {label: "Native", text: "(Learn 15000 words)", description: "Rodily mluvci ma celkem 10000 az 20000 slov v aktivni slovni zasobe. V teto kolekci jsou ty nejznamejsi z nich. (Doporucujeme stahovat az po projiti vsech predchozich kolekci)", disabled: true}
+  ];
+</script>
+
+<style>
+</style>
