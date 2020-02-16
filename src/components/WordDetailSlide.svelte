70ed846fde965c4294a7d5ba5abbc8011b5a3f10
Martin Jablecnik <martin.jablecnik@email.cz>
2020-02-16 20:36:30 +0100

    Add WordDetailSlide

--- src/components/WordDetailSlide.svelte
+++ src/components/WordDetailSlide.svelte
@@ -1,7 +1,58 @@
-  <h1> test </h1>
+<Block>
+  <center>
+      <BlockTitle large>{word.text}</BlockTitle>
+      <Block>
+        <BlockHeader>[ {word.pronunciation} ]  &#x1F509;</BlockHeader>
+      </Block>
+
+      <Block>
+        <Row>
+          <Col width="20">
+          </Col>
+          <Col width="60">
+            <List>
+              {#each word.sense as sense}
+                <ListItem link="#" sheetOpen=".example-sheet"> {sense} </ListItem>
+              {/each}
+            </List>
+          </Col>
+          <Col width="20">
+          </Col>
+        </Row>
+      </Block>
+
+      <Sheet class="example-sheet" opened={sheetOpened} onSheetClosed={() => sheetOpened = false}>
+        <Toolbar>
+          <div class="left"></div>
+          <div class="right">
+            <Link sheetClose>Close</Link>
+          </div>
+        </Toolbar>
+        <!--  Scrollable sheet content -->
+        <PageContent>
+          <Block>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae ducimus dolorum ipsa aliquid accusamus perferendis laboriosam delectus numquam minima animi, libero illo in tempora harum sequi corporis alias ex adipisci.</p>
+            <p>Sunt magni enim saepe quasi aspernatur delectus consectetur fugiat necessitatibus qui sed, similique quis facere tempora, laudantium quae expedita ea, aperiam dolores. Aut deserunt soluta alias magnam. Consequatur, nisi, enim.</p>
+            <p>Eaque maiores ducimus, impedit unde culpa qui, explicabo accusamus, non vero corporis voluptatibus similique odit ab. Quaerat quasi consectetur quidem libero? Repudiandae adipisci vel voluptatum, autem libero minus dignissimos repellat.</p>
+            <p>Iusto, est corrupti! Totam minus voluptas natus esse possimus nobis, delectus veniam expedita sapiente ut cum reprehenderit aliquid odio amet praesentium vero temporibus obcaecati beatae aspernatur incidunt, perferendis voluptates doloribus?</p>
+            <p>Illum id laborum tempore, doloribus culpa labore ex iusto odit. Quibusdam consequuntur totam nam obcaecati, enim cumque nobis, accusamus, quos voluptates, voluptatibus sapiente repellendus nesciunt praesentium velit ipsa illo iusto.</p>
+          </Block>
+        </PageContent>
+      </Sheet>
+  </center>
+</Block>
 
 <script>
-  alert("test");
+  import { 
+    f7, Page, PageContent, 
+    Block, BlockTitle, BlockHeader, 
+    Navbar, Subnavbar,
+    Swiper, SwiperSlide,
+    List, ListItem, AccordionContent,
+    Row, Col, Button, Link,
+    Sheet, Toolbar
+  } from 'framework7-svelte';
+  import { _ } from 'svelte-i18n';
 
   export let word = [];
   let sheetOpened = false;
--- src/views/TrainingView.svelte
+++ src/views/TrainingView.svelte
@@ -1,10 +1,66 @@
 <Page name="Training">
-  <div slot="after">
-    <TestComponent />
-  </div>
+  <Navbar title="LearnEnglishWords">
+  <Subnavbar> 
+    Training Words
+  </Subnavbar> 
+  </Navbar>              
+  <Toolbar position='bottom'>
+    <Link on:click={prevWord}>Previous word</Link>
+    <Link on:click={nextWord}>Next word</Link>
+  </Toolbar>
+
+  <Swiper init navigation params={{speed: 0, allowTouchMove: true, loop: true, followFinger: false}}>
+    {#each $trainingData.words as word, id}
+      <SwiperSlide>
+        <Block>
+          <WordDetailSlide {word}/>
+        </Block>
+      </SwiperSlide>
+    {/each}
+  </Swiper>
+
+
+  <BlockTitle >Uz umis dane slovicko?</BlockTitle>
+  <Row>
+    <Col width="25">
+    </Col>
+    <Col width="25">
+      <Button large fill color="red" on:click={prevWord}>Ne</Button>
+    </Col>
+    <Col width="25">
+      <Button large fill color="green" on:click={nextWord}>Ano</Button>
+    </Col>
+    <Col width="25">
+    </Col>
+  </Row>
+
 </Page>
 
 <script>
-  import { Page } from 'framework7-svelte';
-  import { TestComponent } from './TestComponent.svelte';
+  import { 
+    f7, Page, PageContent, 
+    Block, BlockTitle, BlockHeader, 
+    Navbar, Subnavbar,
+    Swiper, SwiperSlide,
+    List, ListItem, AccordionContent,
+    Row, Col, Button, Link,
+    Sheet, Toolbar
+  } from 'framework7-svelte';
+  import { trainingData } from '../js/store.js';
+  import WordDetailSlide from '../components/WordDetailSlide.svelte';
+  import { _ } from 'svelte-i18n';
+
+
+  function nextWord() {
+    let swiper = f7.swiper.get('.swiper-container')
+    swiper.slideNext();
+  }
+  function prevWord() {
+    let swiper = f7.swiper.get('.swiper-container')
+    swiper.slidePrev();
+  }
 </script>
+
+<style>
+</style>
+
