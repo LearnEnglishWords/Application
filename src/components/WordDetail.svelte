5799382c00bf391c2994eb695cb1e5485d74f7fc
Martin Jablecnik <martin.jablecnik@email.cz>
2020-03-23 09:31:10 +0100

    Add examples

--- src/components/WordDetail.svelte
+++ src/components/WordDetail.svelte
@@ -17,7 +17,7 @@
   </Row>
 </Block>
 
-{#if $trainingData.isTraining}
+<!--{#if $trainingData.isTraining}-->
   <Sheet class="description" opened={descriptionOpened} onSheetClosed={() => descriptionOpened = false}>
     <Toolbar>
       <div class="left"></div>
@@ -28,15 +28,15 @@
     <!--  Scrollable sheet content -->
     <PageContent>
       <Block>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae ducimus dolorum ipsa aliquid accusamus perferendis laboriosam delectus numquam minima animi, libero illo in tempora harum sequi corporis alias ex adipisci.</p>
-        <p>Sunt magni enim saepe quasi aspernatur delectus consectetur fugiat necessitatibus qui sed, similique quis facere tempora, laudantium quae expedita ea, aperiam dolores. Aut deserunt soluta alias magnam. Consequatur, nisi, enim.</p>
-        <p>Eaque maiores ducimus, impedit unde culpa qui, explicabo accusamus, non vero corporis voluptatibus similique odit ab. Quaerat quasi consectetur quidem libero? Repudiandae adipisci vel voluptatum, autem libero minus dignissimos repellat.</p>
-        <p>Iusto, est corrupti! Totam minus voluptas natus esse possimus nobis, delectus veniam expedita sapiente ut cum reprehenderit aliquid odio amet praesentium vero temporibus obcaecati beatae aspernatur incidunt, perferendis voluptates doloribus?</p>
-        <p>Illum id laborum tempore, doloribus culpa labore ex iusto odit. Quibusdam consequuntur totam nam obcaecati, enim cumque nobis, accusamus, quos voluptates, voluptatibus sapiente repellendus nesciunt praesentium velit ipsa illo iusto.</p>
+        <List>
+          {#each word.examples as example, id}
+            <ListItem> {example} <Button>&#x1F509;</Button></ListItem>
+          {/each}
+        </List>
       </Block>
     </PageContent>
   </Sheet>
-{/if}
+<!--{/if}-->
 
 <script>
   import { 
