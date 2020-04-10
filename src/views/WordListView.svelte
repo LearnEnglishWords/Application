9b4e11e1d867ea73713dac4a833a3b5251237885
Martin Jablecnik <martin.jablecnik@email.cz>
2020-04-10 18:12:08 +0200

    Add feature: Saving all words in WordListView at once

--- src/views/WordListView.svelte
+++ src/views/WordListView.svelte
@@ -9,13 +9,19 @@
           <div on:click={() => playSound(word)}>
             {word.text}  &#x1F509;
           </div>
-          <Button raised on:click={() => { setState(word, !isKnown(word)) }}>
+          <Button raised on:click={() => { setState(word, !wordState[word.text]) }}>
             {#if wordState[word.text]} {$_('words_list.unknown')} {:else} {$_('words_list.known')} {/if}
           </Button>
         </ListItem>
       {/each}
     </List>
   </Block>
+  {#if removeWords.length > 0 || addWords.length > 0}
+    <Toolbar position={'bottom'}>
+      <Link></Link>
+      <Link on:click={saveWords}>Ulozit</Link>
+    </Toolbar>
+  {/if}
 </Page>
 
 <script>
@@ -23,7 +29,8 @@
     f7, Page, 
     BlockTitle, Block,
     List, ListItem,
-    Button
+    Button, Link,
+    Toolbar
   } from 'framework7-svelte';
   import Collection from '../js/collection.js';
   import Header from '../components/Header.svelte';
@@ -39,8 +46,12 @@
   import { _ } from 'svelte-i18n';
 
   export let name;
+  export let f7router;
 
   const collection = new Collection();
+  let addWords = [];
+  let removeWords = [];
+  let saved = false;
 
   let wordState = {};
   let allWordsSorted = [];
@@ -61,24 +72,78 @@
     });
   });
 
+  function updateStatistics() {
+    let removeWordModes = [
+      {mode: 'read', prevState: true},
+      {mode: 'write', prevState: true},
+      {mode: 'listen', prevState: true}
+    ];
+    let addWordModes = [
+      {mode: 'read', prevState: false},
+      {mode: 'write', prevState: false},
+      {mode: 'listen', prevState: false}
+    ];
+
+    removeWords.forEach((wordId) => {
+      let word = allWordsSorted.find((w) => w.text === wordId);
+      if (!isKnown(word)) {
+        let prevState = getState(word);
+        word.learning = {"read": true, "write": true, "listen": true};
+        updateAllStatisticsAndSaveWord(word, prevState, [...removeWordModes]);
+      }
+    });
+
+    addWords.forEach((wordId) => {
+      let word = allWordsSorted.find((w) => w.text === wordId);
+      if (isKnown(word)) {
+        let prevState = getState(word);
+        word.learning = {"read": false, "write": false, "listen": false};
+        updateAllStatisticsAndSaveWord(word, prevState, [...addWordModes]);
+      }
+    });
+  }
+
+  function updateCategoryWords() {
+    trainingModes.forEach((mode) => {
+      let wordStorage = $categoryDetailData.wordStorages[mode.value];
+
+      var updateWordIds = wordStorage.allWordIds
+        .filter(wordId => !removeWords.includes(wordId))
+        .concat(addWords);
+
+      wordStorage.reset();
+      wordStorage.allWordIds = [...new Set(updateWordIds)];
+      wordStorage.allWords = [];
+      wordStorage.loadWords();
+      wordStorage.saveWordIds();
+    });
+  }
+
+  function saveWords() {
+    updateStatistics();
+    updateCategoryWords();
+    f7router.back();
+  }
 
   function setState(word, known) {
-    let prevState = getState(word);
-    word.learning = {"read": known, "write": known, "listen": known};
-    wordState[word.text] = isKnown(word);
+    wordState[word.text] = known;
 
-    if(known) {
-      trainingModes.forEach((mode) => $categoryDetailData.wordStorages[mode.value].removeWord(word));
-    } else {
-      trainingModes.forEach((mode) => $categoryDetailData.wordStorages[mode.value].addWord(word));
+    if (isKnown(word) === known) {
+      var index = addWords.indexOf(word.text);
+      if (index > -1) { addWords.splice(index, 1) }
+
+      index = removeWords.indexOf(word.text);
+      if (index > -1) { removeWords.splice(index, 1) }
+      return
     }
 
-    let allModes = [
-      {mode: 'read', prevState: isKnown},
-      {mode: 'write', prevState: isKnown},
-      {mode: 'listen', prevState: isKnown}
-    ];
-    updateAllStatisticsAndSaveWord(word, prevState, allModes);
+    if (known) {
+      removeWords.push(word.text);
+      removeWords = [...removeWords];
+    } else {
+      addWords.push(word.text);
+      addWords = [...addWords];
+    }
   }
 
 </script>
