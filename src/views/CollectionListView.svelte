d6dcd8599b221e9e1063b66850936660b5ed7583
Martin Jablecnik <martin.jablecnik@email.cz>
2020-04-14 14:52:07 +0200

    Create Collection entity and move loadingCategories function into there

--- src/views/CollectionListView.svelte
+++ src/views/CollectionListView.svelte
@@ -3,13 +3,13 @@
   <Block strong inset>
     <BlockTitle medium>{$_('collection.title')}</BlockTitle>
     <List accordionList mediaList inset>
-      {#each collectionItems as {id, title, text, description, active}}
+      {#each collectionItems as {id, title, shortDescription, fullDescription, active}}
         {#if active}
-          <ListItem accordionItem title="{title}" text="{text}">
+          <ListItem accordionItem title="{title}" text="{shortDescription}">
             <AccordionContent>
               <Block>
                 <p>
-                  {description}
+                  {fullDescription}
                 </p>
                 {#if $downloadedCollections.includes(id)}
                   <Button fill on:click={ () => continueButton(id) } color="green">{$_('collection.button.continue')}</Button>
@@ -41,19 +41,20 @@
   import CollectionStorage from '../js/storages/collections.js';
   import DS from '../js/storages/data.js';
   import { WordsType, Modes, AppInfo } from '../js/utils.js'
+  import Collection from '../js/entities/collection.js'
   import Header from '../components/Header.svelte';
   import { allCollectionsData, collectionData, downloadedCollections} from '../js/store.js';
   import { _ } from 'svelte-i18n';
 
   export let f7router;
+
+
   let collectionStorage = new CollectionStorage();
   let counter;
   let progressBarEl;
   let wordsAmount = 0;
   let downloadingCollectionId = null;
-  
-  
-  setTimeout(() => { preloadAllCollections() }, 1000);
+
 
   function preloadAllCollections() {
     if($allCollectionsData.length === 0) {
@@ -98,7 +99,6 @@
 
     if(counter === wordsAmount) {
       updateCollectionIds($downloadedCollections.concat([downloadingCollectionId]));
-      loadCollection(downloadingCollectionId);
       f7.progressbar.hide(progressBarEl); 
       downloadingCollectionId = null;
     }
@@ -116,97 +116,25 @@
   }
 
   function loadCollection(collectionId) {
-    let collection = {
-      "id": collectionId, 
-      "name": getCollection(collectionId).title, 
-      "categories": [],
-      "categoriesWithWords": []
-    }
+    let collection = getCollection(collectionId);
+    collection.loadCategories();
 
-    DS.getCategoryList(collectionId).then((categories) => {
-      categories.forEach((category) => {
-        DS.getWordIdsList(collectionId, category.id, WordsType.ALL, Modes.ALL).then((wordIds) => {
-          collection.categoriesWithWords.push({"category": category, "wordIds": wordIds})
-        });
-
-        DS.getCategoryStatistics(collectionId, category.id).then((stats) => {
-          if (stats !== null) {
-            category.stats = stats;
-            category.active = false;
-            DS.getCategoryModeStatistics(collectionId, category.id)
-              .then((modeStats) => { 
-                category.modeStats = modeStats; 
-              });
-            collection.categories.push(category);
-          }
-        });
-      });
-    });
     const index = $allCollectionsData.findIndex((c) => c.id === collectionId);
     if (index > -1) { $allCollectionsData.splice(index, 1) }
 
     $allCollectionsData.push(collection);
   }
 
-  const collectionItems = [
-    {
-      id: 2,
-      title: $_('collection.items.basic.title'), 
-      text: $_('collection.items.basic.text'), 
-      description: $_('collection.items.basic.description'), 
-      mainCategory: $_('collection.items.basic.main_category'), 
-      active: true
-    },
-    {
-      id: 7,
-      title: $_('collection.items.standard.title'), 
-      text: $_('collection.items.standard.text'), 
-      description: $_('collection.items.standard.description'), 
-      mainCategory: $_('collection.items.standard.main_category'), 
-      active: true
-    },
-    {
-      id: 3,
-      title: $_('collection.items.advanced.title'), 
-      text: $_('collection.items.advanced.text'), 
-      description: $_('collection.items.advanced.description'), 
-      mainCategory: $_('collection.items.advanced.main_category'), 
-      active: true
-    },
-    {
-      id: 9,
-      title: $_('collection.items.category.title'), 
-      text: $_('collection.items.category.text'), 
-      description: $_('collection.items.category.description'), 
-      active: true
-    },
-    {
-      id: "student",
-      title: "Student",
-      text: $_('collection.items.student.text'), 
-      description: $_('collection.items.student.description'), 
-      active: false
-    },
-    {
-      id: "native",
-      title: "Native", 
-      text: $_('collection.items.native.text'), 
-      description: $_('collection.items.native.description'), 
-      active: false
-    },
-    {
-      id: "media",
-      title: "Media",
-      text: $_('collection.items.media.text'), 
-      description: $_('collection.items.media.description'), 
-      active: false
-    },
-    {
-      id: "personal",
-      title: "Personal",
-      text: $_('collection.items.personal.text'), 
-      description: $_('collection.items.personal.description'), 
-      active: false
-    }
+  let collectionItems = [
+    new Collection(2, 'basic', true),
+    new Collection(7, 'standard', true),
+    new Collection(3, 'advanced', true),
+    new Collection(9, 'category', true),
+    new Collection("student", 'student', false),
+    new Collection("native", 'native', false),
+    new Collection("media", 'media', false),
+    new Collection("personal", 'personal', false),
   ];
+  
+  setTimeout(() => { preloadAllCollections() }, 1000);
 </script>
