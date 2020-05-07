6183823a3b5700f079f666d0e847dab7301c5e9f
Martin Jablecnik <martin.jablecnik@email.cz>
2020-05-07 13:19:50 +0200

    Remove comment with icon names

--- src/views/CategoryListView.svelte
+++ src/views/CategoryListView.svelte
@@ -32,29 +32,6 @@
     <List class="list-categories">
       {#each $collectionData.categoryGroup.categories as category, id}
         <ListItem class="list-item" title="{category.title}" on:click="{() => toggleCategory(category)}">
-          <!-- 
-            "zvířata": "paw",
-            "tělo": "male",
-            "město": "new-construction",
-            "práce a zaměstnání": "bag",
-            "auto": "car-2",
-            "oblečení": "shorts",
-            "barvy": "palette",
-            "lidé": "multiple-11",
-            "jídlo": "cutlery",
-            "dům a zahrada": "fence",
-            "zdraví": "heartbeat",
-            "zákon a pořádek": "law",
-            "matematika": "math",
-            "hudba": "music",
-            "škola": "school",
-            "věda a výzkum": "flask-2",
-            "sport": "soccer-ball",
-            "datum a čas": "time-clock",
-            "cestování": "map-pin",
-            "počasí": "sun-fog-43",
-            "ostatní": "trash",
-          -->
           <div slot="media" class="item-media">
             <SVGIcon name="{category.icon}" size="24" tag="list" />
           </div>
