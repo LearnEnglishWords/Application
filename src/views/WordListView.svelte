3f5aed3c91f75edf092dad97ea258ba746d59328
Martin Jablecnik <martin.jablecnik@email.cz>
2020-05-17 13:00:42 +0200

    Fix WordList items

--- src/views/WordListView.svelte
+++ src/views/WordListView.svelte
@@ -70,13 +70,16 @@
       items: allWords,
       itemTemplate:
       `<li class="list-item word-item">
-        <label class="item-checkbox item-content">
-          <input type="checkbox" {{checked}} class="wordbox">
-          <i class="icon icon-checkbox"></i>
-          <div class="item-inner">
-           <div class="item-title">{{word.text}}<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" width="24" height="24" class="mode-icon icon-24-volume play-sound" stroke-width="2"><g transform="translate(0, 0)"><polygon stroke-linecap="square" stroke-miterlimit="10" points="14,22 6,16 1,16 1,8 6,8 14,2" stroke-linejoin="miter"></polygon><line stroke-linecap="square" stroke-miterlimit="10" x1="19" y1="12" x2="23" y2="12" stroke-linejoin="miter"></line><line stroke-linecap="square" stroke-miterlimit="10" x1="17.7" y1="7" x2="21.1" y2="5" stroke-linejoin="miter"></line><line stroke-linecap="square" stroke-miterlimit="10" x1="17.7" y1="17" x2="21.1" y2="19" stroke-linejoin="miter"></line></g></svg></div>     
-          </div>
-        </label>
+        <div class="item-inner">
+         <div class="item-title">     
+           <label class="item-checkbox">
+             <input type="checkbox" {{checked}} class="wordbox">
+             <i class="icon icon-checkbox"></i>
+            {{word.text}}
+           </label>
+           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" width="24" height="24" class="mode-icon icon-24-volume play-sound" stroke-width="2"><g transform="translate(0, 0)"><polygon stroke-linecap="square" stroke-miterlimit="10" points="14,22 6,16 1,16 1,8 6,8 14,2" stroke-linejoin="miter"></polygon><line stroke-linecap="square" stroke-miterlimit="10" x1="19" y1="12" x2="23" y2="12" stroke-linejoin="miter"></line><line stroke-linecap="square" stroke-miterlimit="10" x1="17.7" y1="7" x2="21.1" y2="5" stroke-linejoin="miter"></line><line stroke-linecap="square" stroke-miterlimit="10" x1="17.7" y1="17" x2="21.1" y2="19" stroke-linejoin="miter"></line></g></svg></div>     
+         </div>
+        </div>
       </li>`,
       height: f7.theme === 'ios' ? 50 : (f7.theme === 'md' ? 60 : 33),
     });
