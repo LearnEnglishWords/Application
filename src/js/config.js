f6a18c19686e680b6360c2f649d44a29d3421bdf
Martin Jablecnik <martin.jablecnik@email.cz>
2020-05-26 21:03:29 +0200

    Change production variable and bump version

--- src/js/config.js
+++ src/js/config.js
@@ -1,8 +1,8 @@
 export const appName = "LearnEnglishWords";
 export const appId = "'com.learnenglishwords'";
-export const version = "3.2.0";
+export const version = "3.2.1";
 
-export const isProduction = false;
+export const isProduction = true;
 
 export const feedbackEmail = "feedback@learn-english-words.eu";
 export const infoEmail = "info@learn-english-words.eu";
