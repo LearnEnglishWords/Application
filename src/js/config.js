46f0f48ada7c6c652933799d6c424915428cc8ae
Martin Jablecnik <martin.jablecnik@email.cz>
2020-06-15 14:15:17 +0200

    Bump version to 3.2.3

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 export const appName = "LearnEnglishWords";
 export const appId = "'com.learnenglishwords'";
-export const version = "3.2.2";
+export const version = "3.2.3";
 
 export const isProduction = true;
 
