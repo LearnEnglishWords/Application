110852b622ea28d46994ab6b4314fe5dad9ce378
TheCzechTayen <TheCzechTayen@gmail.com>
2020-06-21 19:52:05 +0200

    já nevím už

--- src/views/CategoryDetailView.svelte
+++ src/views/CategoryDetailView.svelte
@@ -83,18 +83,18 @@
     </Row>
     <Row class="{currentTestingMode !== null ? currentTestingMode : ''}">
       <Col>
-        <p class="{currentTestingMode === 'training' ? 'selected' : ''}">
-          Slouží jenom k učení a procvičování samotných sloviček nanečisto. 
-          Výsledky se nezapočítávají do statistik.
-        </p>
-        <p class="{currentTestingMode === 'exam' ? 'selected' : ''}">
-          Slouží k otestování, zda daná slovíčka již umíte a opravdu si je pamatujete. 
-          Výsledky se započítávají do statistik a známá slovíčka se již v jednotlivých módech znovu nezobrazují.
-        </p>
-        <p class="{currentTestingMode === 'repetition' ? 'selected' : ''}">
-          Slouží ke kontrole, zda si již jednou naučená slovíčka stále pamatujete i o několik dní či týdnů později. 
-          Výsledky se zapocitavaji do statistik a slovíčka, která máte špatně se vrací zpátky do procesu učení.
-          Naopak slovíčka, která zodpovíte správně se zde již znovu nezobrazí. 
+        <p class="{currentTestingMode !== null ? currentTestingMode : ''}">
+          {#if currentTestingMode === 'training'}
+            Slouží jenom k učení a procvičování samotných sloviček nanečisto. 
+            Výsledky se nezapočítávají do statistik.
+          {:else if currentTestingMode === 'exam'}
+            Slouží k otestování, zda daná slovíčka již umíte a opravdu si je pamatujete. 
+            Výsledky se započítávají do statistik a známá slovíčka se již v jednotlivých módech znovu nezobrazují.
+          {:else if currentTestingMode === 'repetition'}
+            Slouží ke kontrole, zda si již jednou naučená slovíčka stále pamatujete i o několik dní či týdnů později. 
+            Výsledky se zapocitavaji do statistik a slovíčka, která máte špatně se vrací zpátky do procesu učení.
+            Naopak slovíčka, která zodpovíte správně se zde již znovu nezobrazí. 
+          {/if} 
         </p>
         <Button on:click={goToTrainingView}>{$_('category.buttons.start')}</Button>
       </Col>
