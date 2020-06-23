25593c3c594e3f9bec6ffd0ba884ee8675092863
Martin Jablecnik <martin.jablecnik@email.cz>
2020-06-23 19:33:11 +0200

    Remove if

--- src/views/CategoryDetailView.svelte
+++ src/views/CategoryDetailView.svelte
@@ -81,21 +81,21 @@
         <span>{$_('category.buttons.training')}</span>
       </Col>
     </Row>
-    <Row noGap class="{currentTestingMode !== null ? currentTestingMode : ''}">
+    <Row class="{currentTestingMode !== null ? currentTestingMode : ''}">
       <Col>
-        <p>
-          {#if currentTestingMode === 'training'}
-            Slouží jenom k učení a procvičování slovíček nanečisto. 
-            Výsledky se nezapočítávají do statistik.
-          {:else if currentTestingMode === 'exam'}
-            Slouží k otestování, zda daná slovíčka již umíte a opravdu si je pamatujete. 
-            Výsledky se započítávají do statistik a známá slovíčka se zde v přezkoušení již znovu nezobrazí.
-          {:else if currentTestingMode === 'repetition'}
-            Slouží ke kontrole, zda si již jednou naučená slovíčka stále ještě pamatujete i po několika dnech či týdnech. 
-            Výsledky se započítávají do statistik a slovíčka, která máte špatně se vrací zpátky do procesu učení.
-            Naopak slovíčka která zodpovíte správně, se zde již znovu nezobrazí. 
-          {/if}
-        </p> 
+        <p class="{currentTestingMode === 'training' ? 'selected' : ''}">
+          Slouží jenom k učení a procvičování slovíček nanečisto. 
+          Výsledky se nezapočítávají do statistik.
+        </p>
+        <p class="{currentTestingMode === 'exam' ? 'selected' : ''}">
+          Slouží k otestování, zda daná slovíčka již umíte a opravdu si je pamatujete. 
+          Výsledky se započítávají do statistik a známá slovíčka se zde v přezkoušení již znovu nezobrazí.
+        </p>
+        <p class="{currentTestingMode === 'repetition' ? 'selected' : ''}">
+          Slouží ke kontrole, zda si již jednou naučená slovíčka stále ještě pamatujete i po několika dnech či týdnech. 
+          Výsledky se započítávají do statistik a slovíčka, která máte špatně se vrací zpátky do procesu učení.
+          Naopak slovíčka která zodpovíte správně, se zde již znovu nezobrazí. 
+        </p>
         <Button on:click={goToTrainingView}>{$_('category.buttons.start')}</Button>
       </Col>
     </Row>
