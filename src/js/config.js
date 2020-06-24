eb990a8201f4345f547135cdda615348019ae884
Martin Jablecnik <martin.jablecnik@email.cz>
2020-06-24 10:07:42 +0200

    Bump devel version

--- src/js/config.js
+++ src/js/config.js
@@ -1,8 +1,8 @@
 export const appName = "LearnEnglishWords";
 export const appId = "'com.learnenglishwords'";
-export const version = "3.2.4";
+export const version = "3.2.5-devel";
 
-export const isProduction = true;
+export const isProduction = false;
 
 export const feedbackEmail = "feedback@learn-english-words.eu";
 export const infoEmail = "info@learn-english-words.eu";
