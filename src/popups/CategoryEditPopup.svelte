8e8732d175bc8079ae9ec258190acc0a4d7f97c8
Martin Jablecnik <martin.jablecnik@email.cz>
2020-06-30 15:21:22 +0200

    Fix changeDialog animation

--- src/popups/CategoryEditPopup.svelte
+++ src/popups/CategoryEditPopup.svelte
@@ -11,16 +11,16 @@
   </div>
   <div class="personal-panel">
     <Row noGap>
-      <Col class="ripple {activeDialog === ActiveCategoryDialog.REMOVE ? 'active' : 'inactive'}" on:click={() => activeDialog = activeDialog !== ActiveCategoryDialog.REMOVE ? ActiveCategoryDialog.REMOVE : null}>
+      <Col class="ripple {activeDialog === ActiveCategoryDialog.REMOVE ? 'active' : 'inactive'}" on:click={() => changeDialog(ActiveCategoryDialog.REMOVE)}>
         <SVGIcon element="navigation" name="delete-forever" size="16" />
         <span>Smazat</span>
       </Col>
-      <Col class="ripple {activeDialog === ActiveCategoryDialog.ADD_WORD ? 'active' : 'inactive'}" on:click={() => activeDialog = activeDialog !== ActiveCategoryDialog.EDIT ? ActiveCategoryDialog.EDIT : null}>
+      <Col class="ripple {activeDialog === ActiveCategoryDialog.ADD_WORD ? 'active' : 'inactive'}" on:click={() => changeDialog(ActiveCategoryDialog.EDIT)}>
         <SVGIcon element="navigation" name="pen-01" size="16" />
         <span>Přejmenovat</span>
       </Col>
     </Row>
-    <div class="ripple button-add {activeDialog !== null ? 'active' : 'inactive'}" on:click={() => activeDialog = activeDialog !== ActiveCategoryDialog.ADD_WORD ? ActiveCategoryDialog.ADD_WORD : null}>
+    <div class="ripple button-add {activeDialog !== null ? 'active' : 'inactive'}" on:click={() => changeDialog(ActiveCategoryDialog.ADD_WORD)}>
       <SVGIcon element="navigation" name="e-add" size="16" />
     </div>
     <Row noGap class="{activeDialog !== null ? 'opened' : ''}">
@@ -66,5 +66,16 @@
       EDIT: 'edit',
       ADD_WORD: 'add_word',
       REMOVE: 'remove'
+  }
+  
+  function changeDialog(newActiveDialog) {
+    if (activeDialog === null) {
+      activeDialog = newActiveDialog;
+    } else if (activeDialog === newActiveDialog) {
+      activeDialog = null;
+    } else {
+      activeDialog = null;
+      setTimeout(() => { activeDialog = newActiveDialog }, 500);
+    }
   }
 </script>
