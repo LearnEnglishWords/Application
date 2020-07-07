a18dfbcb79864beec0f71093ad3bd8717dc963af
TheCzechTayen <TheCzechTayen@gmail.com>
2020-07-07 16:30:46 +0200

    search prototype

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -8,7 +8,7 @@
     <slot name="title"></slot>
     <NavRight>
       <Link popupOpen=".search">
-        Hledani je tady ty vole :D Klikni sem nebo se koukni do kodu ;) Hlavne sem dej nejakou ikonku, ktera by se tu i zobrazovala a byla tak i videt.. xD
+        <SVGIcon element="navbar" name="magnifier" size="24" />
       </Link>
       <Link popoverOpen=".popover-menu">
         <SVGIcon element="navbar" name="menu-8" size="24" />
--- src/popups/SearchPopup.svelte
+++ src/popups/SearchPopup.svelte
@@ -1,25 +1,42 @@
 <Popup class={name} animate={true}>
   <Header type="popup" popupName={name} title="Vyhledávání" />
+  <span class="search-text">Zadejte anglické slovíčko, které si přejete vyhledat:</span>
 
-  Zadejte anglické slovíčko, které si přejete vyhledat:
+
     <input bind:value={searchInput} type="text" autocomplete="off" placeholder="">
-    <Button on:click={search}>
+    <Button class="search-button" on:click={search}>
       Vyhledat
     </Button>
 
     {#if found === false}
-      zadne takove slovicko nebylo nalezeno
+      <span class="search-failed">Požadované slovíčko nebylo nalezeno</span>
     {:else if found}
-      bylo nalezeno nasledujici slovicko:
-      <WordReadDetail {word} />
+      <div class="word-info">
+        <WordReadDetail {word} />
+      </div>
     {/if}
 
-    <Footer />
+    <div class="search-bar">
+      <Row noGap>
+        <Col class="ripple">
+          <SVGIcon element="navigation" name="paper" size="16" />
+          <span>Příklady</span>
+        </Col>
+        <Col class="ripple">
+          <SVGIcon element="navigation" name="pen-01" size="16" />
+          <span>Upravit</span>
+        </Col>
+        <Col class="ripple">
+          <SVGIcon element="navigation" name="event-confirm" size="16" />
+          <span>Uložit</span>
+        </Col>
+      </Row>
+    </div>
 </Popup>
 
 <script>
   import {
-    Block, Popup, Button
+    Block, Popup, Button, Col, Row
   } from 'framework7-svelte';
   import Header  from '../components/Header.svelte';
   import Footer  from '../components/Footer.svelte';
