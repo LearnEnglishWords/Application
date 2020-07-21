ed56199ba6e35aabfd24e915351ce20d02d264cf
Martin Jablecnik <martin.jablecnik@email.cz>
2020-07-21 11:53:17 +0200

    Enable arrows only for training read mode

--- src/views/TrainingView.svelte
+++ src/views/TrainingView.svelte
@@ -23,7 +23,7 @@
             </div>
           {/each}
         </div>
-        {#if $trainingData.isTraining}
+        {#if $trainingData.isTraining && $trainingData.mode === 'read'}
           <div class="swiper-button-prev" on:click={swiper.slidePrev}><SVGIcon name="ctrl-left" size="24"/></div>
           <div class="swiper-button-next" on:click={swiper.slideNext}><SVGIcon name="ctrl-right" size="24"/></div>
         {/if}
