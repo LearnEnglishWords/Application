02dbfac708eb746879d401642503c28cd829111b
Martin Jablecnik <martin.jablecnik@email.cz>
2020-07-22 11:37:58 +0200

    Add CreateButton into category components

--- src/components/category/Buttons.svelte
+++ src/components/category/Buttons.svelte
@@ -19,12 +19,7 @@
       {/if}
     </Col>
     <Row noGap class="{$categoryData.categoryDialogOpened ? "opened" : "closed"}">
-      <span class="category-name">{$_('category_list.new')}</span>
-      <input bind:value={$categoryData.newCategoryText} on:keydown={(e) => e.key === "Enter" ? createCategory() : null} type="text" autocomplete="off" placeholder=""/>
-      <div class="wrapper">
-        <Button on:click={closeCategoryDialog}>{$_('category_list.buttons.cancel')}</Button>
-        <Button on:click={createCategory}>{$_('category_list.buttons.confirm')}</Button>
-      </div>
+      <CreateButton {f7router} />
     </Row>
   </div>
 
@@ -42,30 +37,11 @@
   import { Button, Row, Col } from 'framework7-svelte';
   import { collectionData, categoryData } from '../../js/store.js';
   import { Collections } from '../../js/utils.js';
-  import CollectionStorage from '../../js/storages/collections.js';
-  import Category from '../../js/entities/category.js';
+  import CreateButton from './CreateButton.svelte';
   import { createEventDispatcher } from 'svelte';
   import { _ } from 'svelte-i18n';
 
   export let f7router;
 
   const dispatch = createEventDispatcher();
-
-  function createCategory() {
-    if ($categoryData.newCategoryText === "") { return }
-
-    let collectionStorage = new CollectionStorage();
-    collectionStorage.createPersonalCategory($categoryData.newCategoryText).then((cat) => {
-      let category = new Category(cat.id, cat.collectionId, cat.name, cat.czechName, cat.icon);  
-      $collectionData.categoryGroup.push(category);
-    })
-
-    setTimeout(() => { f7router.refreshPage() }, 500);
-  }
-
-  function closeCategoryDialog() {
-    $categoryData.newCategoryText = "";
-    $categoryData.categoryDialogOpened = false;
-  }
-
 </script>
