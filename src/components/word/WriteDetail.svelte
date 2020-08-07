02be3e60e05de15ccd0656d95b0365bbcf5f13ce
Martin Jablecnik <martin.jablecnik@email.cz>
2020-08-07 09:08:32 +0200

    Add showing ReadWord in WriteDetail

--- src/components/word/WriteDetail.svelte
+++ src/components/word/WriteDetail.svelte
@@ -1,31 +1,21 @@
-<div class="other-mode">
-  <div class="other-div word">
-    {#if mode === "write"} 
-      {#each word.sense.slice(0,4) as sense, id}
-        {sense.toLowerCase()}{#if id + 1 !== word.sense.length},{/if} <br/>
-      {/each}
-    {:else if mode === "listen"} 
-      <div class="read-mode" on:click={() => playTextSound(word.text, $settingsData.pronunciation)}>
-        <div class="read-title">{$_('training.listen_title')}</div>
-        <SVGIcon name="volume" size="24"/>
-      </div>
-    {/if}
-  </div>
-  <div class="footer-container footer-singular arrows">
-    <div class="footer-content arrows">
-      {#if result === null}
-        <Button class="page-button button-examples" on:click={checkButton}>
-          {translatedText.length === 0 && $trainingData.isTraining ? $_('training.buttons.unknown') : $_('training.buttons.check')}
-        </Button>
-      {:else}
-        {#if !showSense && mode === "listen"}
-          <Button class="page-button button-examples" on:click={() => showSense = true }>{$_('training.buttons.show_sense')}</Button>
-        {/if}
-        <Button class="page-button button-examples" on:click={() => dispatch('nextWord')}>{$_('training.buttons.continue')}</Button>
+{#if showSense}
+  <ReadWord {word} />
+{:else}
+  <div class="other-mode">
+    <div class="other-div word">
+      {#if mode === "write"} 
+        {#each word.sense.slice(0,4) as sense, id}
+          {sense.toLowerCase()}{#if id + 1 !== word.sense.length},{/if} <br/>
+        {/each}
+      {:else if mode === "listen"} 
+        <div class="read-mode" on:click={() => playTextSound(word.text, $settingsData.pronunciation)}>
+          <div class="read-title">{$_('training.listen_title')}</div>
+          <SVGIcon name="volume" size="24"/>
+        </div>
       {/if}
     </div>
   </div>
-</div>
+{/if}
 
 <div class="page-title">
   {showSense ? $_('training.sense_title') : ""}
@@ -67,6 +57,21 @@
   </div>
 {/if}
     
+<div class="footer-container footer-singular arrows">
+  <div class="footer-content arrows">
+    {#if result === null}
+      <Button class="page-button button-examples" on:click={checkButton}>
+        {translatedText.length === 0 && $trainingData.isTraining ? $_('training.buttons.unknown') : $_('training.buttons.check')}
+      </Button>
+    {:else}
+      {#if !showSense && mode === "listen"}
+        <Button class="page-button button-examples" on:click={() => showSense = true }>{$_('training.buttons.show_sense')}</Button>
+      {/if}
+      <Button class="page-button button-examples" on:click={() => dispatch('nextWord')}>{$_('training.buttons.continue')}</Button>
+    {/if}
+  </div>
+</div>
+
 
 <script>
   import { Button } from 'framework7-svelte';
@@ -74,6 +79,7 @@
   import { _ } from 'svelte-i18n';
   import SVGIcon from '../SVGIcon.svelte';
   import SenseList from './SenseList.svelte';
+  import ReadWord from './ReadWord.svelte';
   import { playTextSound, LearningMode } from '../../js/utils.js';
   import { trainingData, statisticsData, settingsData } from '../../js/store.js';
 
