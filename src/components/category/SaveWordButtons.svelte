690b5a7e53b1d98b855f4fc2ac5edfcd50b502ad
Martin Jablecnik <martin.jablecnik@email.cz>
2020-08-10 16:05:46 +0200

    Remove button for add categories from category list for save words

--- src/components/category/SaveWordButtons.svelte
+++ src/components/category/SaveWordButtons.svelte
@@ -1,14 +1,9 @@
-<div class="personal-navigation {$categoryData.categoryDialogOpened ? "opened" : ""}">
-  <Col class="ripple add-button active" on:click={() => $categoryData.selectedCategories.length === 0 ? $categoryData.categoryDialogOpened = !$categoryData.categoryDialogOpened : null}>
-    {#if $categoryData.selectedCategories.length > 0}
-      <Button class="page-button button-next" on:click={saveClick}>{$_('category_list.buttons.save')}</Button>
-    {:else}
-      <Button class="add">{$_('category_list.buttons.add')}</Button>
-    {/if}
+<div class="personal-navigation">
+  <Col class="ripple add-button active">
+    <Button on:click={saveClick} class="page-button button-next button-trans {$categoryData.selectedCategories.length === 0 ? 'button-gray' : ''}">
+      {$_('category_list.buttons.save')}
+    </Button>
   </Col>
-  <Row noGap class="{$categoryData.categoryDialogOpened ? "opened" : "closed"}">
-    <CreateButton {f7router} />
-  </Row>
 </div>
 
 <script>
@@ -23,6 +18,11 @@
 
  
   function saveClick() {
+    if ($categoryData.selectedCategories.length === 0) {
+      openDialog(f7, $_('dialog.unselected_category.text'));
+      return
+    }
+
     for (let category of $categoryData.selectedCategories) {
       category.addWord(saveWord); 
     }
