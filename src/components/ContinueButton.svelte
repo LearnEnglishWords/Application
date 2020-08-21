7d8db294dd3f5ad0141b1f1e1f4846a499afdd7a
Martin Jablecnik <martin.jablecnik@email.cz>
2020-08-21 17:38:21 +0200

    Add ContinueButton component

---
+++ src/components/ContinueButton.svelte
@@ -0,0 +1,11 @@
+<slot></slot>
+<div class="footer-container footer-singular">
+  <div class="footer-content">
+    <Button on:click class="page-button button-next">{$_('buttons.continue')}</Button>
+  </div>
+</div>
+
+<script>
+  import { Button } from 'framework7-svelte';
+  import { _ } from 'svelte-i18n';
+</script>
