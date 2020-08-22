363f8bf032c6d087c83b2d30ee859ee53748619a
Martin Jablecnik <martin.jablecnik@email.cz>
2020-08-22 13:54:12 +0200

    Change emails and add author name

--- src/js/config.js
+++ src/js/config.js
@@ -3,12 +3,13 @@ import { isDevel } from './local-config.js'
 export const appName = "LearnEnglishWords";
 export const appId = "'com.learnenglishwords'";
 export const version = "3.4.4";
+export const author = "Martin Jablečník";
 
 export const isProduction = !isDevel;
 
-export const feedbackEmail = "feedback@learn-english-words.eu";
-export const infoEmail = "info@learn-english-words.eu";
-export const errorEmail = "error@learn-english-words.eu";
+export const feedbackEmail = "feedback@learn-english-words.net";
+export const infoEmail = "info@learn-english-words.net";
+export const errorEmail = "error@learn-english-words.net";
 
 export const backendUrl = 'https://learn-english-words.eu';
 export const backendApiUrl = 'https://api.learn-english-words.eu';
