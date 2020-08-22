66f1c1ad144e8899d9936d0ce89b94df25c43e46
Martin Jablecnik <martin.jablecnik@email.cz>
2020-08-22 13:54:12 +0200

    Change emails and add author name

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,9 @@
 <div class="footer-popup">
   <span>{$_('about.version')} {version}</span>
-  <span>Copyright &copy; {appName} 2020</span>
+  <span>Copyright &copy; {author} 2020</span>
 </div>
 
 <script>
   import { _ } from 'svelte-i18n';
-  import { version, appName }  from '../js/config.js';
+  import { version, author }  from '../js/config.js';
 </script>
