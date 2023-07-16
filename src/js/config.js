10d01a63aabbc9258b41e2ff5e4074610912f884
Martin Jablečník <martin.jablecnik@email.cz>
2023-07-17 00:09:51 +0200

    Add new backend url

--- src/js/config.js
+++ src/js/config.js
@@ -2,7 +2,7 @@ import { isDevel } from './local-config.js'
 
 export const appName = "LearnEnglishWords";
 export const appId = "'com.learnenglishwords'";
-export const version = "3.6.0";
+export const version = "3.6.1";
 export const author = "Martin Jablečník";
 
 export const isProduction = !isDevel;
@@ -11,6 +11,6 @@ export const feedbackEmail = "feedback@learn-english-words.net";
 export const infoEmail = "info@learn-english-words.net";
 export const errorEmail = "error@learn-english-words.net";
 
-export const backendUrl = 'https://learn-english-words.eu';
-export const backendApiUrl = 'https://api.learn-english-words.eu';
+export const backendUrl = 'https://jablecnik.com';
+export const backendApiUrl = 'https://jablecnik.com';
 export const numberFilteringWords = 30;
